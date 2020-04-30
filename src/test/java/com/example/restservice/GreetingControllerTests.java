5ecab128515ccddbe5dbd96b23763cf42488c97e
Gabriela Kersul <gabriela.kersul@hotmail.com>
2020-04-30 18:13:42 -0300

    deleting rest service folder

---
+++ src/test/java/com/example/restservice/GreetingControllerTests.java
@@ -0,0 +1,37 @@
+package com.example.restservice;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class GreetingControllerTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void noParamShouldReturnDefaultMessageTest() throws Exception {
+
+		this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
+				.andExpect(jsonPath("$.message").value("Hello, World!"));
+	}
+
+	@Test
+	public void paramShouldReturnTailoredMessageTest() throws Exception {
+
+		this.mockMvc.perform(get("/greeting").param("name", "Gabi"))
+				.andDo(print()).andExpect(status().isOk())
+				.andExpect(jsonPath("$.message").value("Hello, Gabi!"));
+	}
+
+}
